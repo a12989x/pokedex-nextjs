568d4ba712e72f101841b3893a43d53ffb567c27
Alexis Guzman <alexis12989x@gmail.com>
2021-01-19 15:26:13 -0600

    Set up Home component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { getListPokemon } from '../lib/api';
 
-const Home = () => {
+import PokemanListItem from '../components/PokemanListItem';
+
+export const getStaticProps = async () => {
+    try {
+        const pokemon = await getListPokemon();
+
+        return { props: { pokemon } };
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const Home = ({ pokemon }) => {
     return (
-        <div>
-            <p>Home Component</p>
-        </div>
+        <main className='home'>
+            <h1 className='home__title'>Pokedex</h1>
+            <ul className='home__pokeman'>
+                {pokemon.map((pokeman, index) => (
+                    <PokemanListItem key={index} index={++index} {...pokeman} />
+                ))}
+            </ul>
+        </main>
     );
 };
 
