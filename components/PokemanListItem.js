3d03f47d039589bd64aada11323935005fc455e7
Alexis Guzman <alexis12989x@gmail.com>
2021-01-19 15:25:42 -0600

    Set up PostListItem component

--- components/PokemanListItem.js
+++ components/PokemanListItem.js
@@ -1,8 +1,23 @@
-const PokemanListItem = () => {
+import Link from 'next/link';
+import Image from 'next/image';
+
+const PokemanListItem = ({ image, name, index }) => {
     return (
-        <div>
-            <p>PokemanListItem Component</p>
-        </div>
+        <li className='pokemanListItem'>
+            <Link href={`/pokeman?id=${index}`}>
+                <a className='pokemanListItem__link'>
+                    <Image
+                        src={image}
+                        alt={name}
+                        width='200'
+                        height='200'
+                        layout='responsive'
+                    />
+                    <span>{index}.</span>
+                    {name}
+                </a>
+            </Link>
+        </li>
     );
 };
 
